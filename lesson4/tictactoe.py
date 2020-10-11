888903768681d9583c1841d60fcb5c758444cad5
Mikhail <mykhail.nakonechnyi.com>
2020-10-11 12:04:47 +0300

    added game structure

--- lesson4/tictactoe.py
+++ lesson4/tictactoe.py
@@ -29,9 +29,9 @@ def inputPlayerLetter():
 
 def choiceRandom():
     if random.randint(0, 1) == 0:
-        return ['X', 'O'] # 'Bot'
+        return ['X', 'O'] # [player, bot]
     else:
-        return ['O', 'X'] # 'Player'
+        return ['O', 'X'] # [player, bot]
 
 def choiceX():
     return ['X', 'O'] # [player, bot]
@@ -41,6 +41,9 @@ def choiceO():
 
 def printWelcome():
     menu = """
+    Welcome to 'Tic-Tac-Toe'
+
+    Please, choose your character/symbol.
     Menu:
     1. Choice X
     2. Choice O
@@ -63,6 +66,126 @@ def printWelcome():
     else:
         return -1 # ERROR
 
-choices = printWelcome()
-print('Player goes', choices[0])
-print('Bot goes', choices[1])
+
+# print('Player goes', choices[0])
+# print('Bot goes', choices[1])
+
+def whoGoesFirst():
+    if random.randint(0, 1) == 0:
+        return 'Player'
+    else:
+        return 'Bot'
+
+
+"""
+ _ _ _
+|1|2|3|
+ - - -
+|4|5|6|
+ - - -
+|7|8|9|
+
+
+1
+ _ _ _
+|x|x|x|
+ - - -
+| | | |
+ - - -
+| | | |
+
+2
+ _ _ _
+| | | |
+ - - -
+|x|x|x|
+ - - -
+| | | |
+
+3
+ _ _ _
+| | | |
+ - - -
+| | | |
+ - - -
+|x|x|x|
+
+4
+ _ _ _
+|x| | |
+ - - -
+|x| | |
+ - - -
+|x| | |
+
+5
+ _ _ _
+| |x| |
+ - - -
+| |x| |
+ - - -
+| |x| |
+
+6
+ _ _ _
+| | |x|
+ - - -
+| | |x|
+ - - -
+| | |x|
+
+7
+ _ _ _
+|x| | |
+ - - -
+| |x| |
+ - - -
+| | |x|
+
+8
+ _ _ _
+| | |x|
+ - - -
+| |x| |
+ - - -
+|x| | |
+
+ 0[1]             [9]
+|-|x| | |x| | |x| | |
+"""
+
+def isWinner(board, letter):
+    return (
+        (board[1] == letter and board[2] == letter and board[3] == letter) or 
+        (board[4] == letter and board[5] == letter and board[6] == letter) or
+        (board[7] == letter and board[8] == letter and board[9] == letter) or
+        (board[1] == letter and board[4] == letter and board[7] == letter) or
+        (board[2] == letter and board[5] == letter and board[8] == letter) or
+        (board[3] == letter and board[6] == letter and board[9] == letter) or
+        (board[1] == letter and board[5] == letter and board[9] == letter) or
+        (board[3] == letter and board[5] == letter and board[7] == letter) 
+    )
+
+def makeMove(board, letter, move):
+    board[move] = letter
+
+
+
+def main():
+    print('test run')
+    while True:
+        theBoard = [' '] * 10
+
+        playerLetter, botLetter = printWelcome()
+
+        gameIsPlaying = True
+
+        while gameIsPlaying:
+            # code for playing
+
+            print('Do you want to play again? (yes/no)')
+            if not (input().lower().startswith('y') or input().lower().startswith('Y')):
+                break
+
+if __name__ == '__main__':
+    main()
