cb0cc226725fc06a6cde2fa0d6b0b134cb7dc6e6
mike <mykhail.nakonechnyi@gmail.com>
2021-03-21 12:12:02 +0200

    repeat task

---
+++ lesson26/repeat.py
@@ -0,0 +1,22 @@
+# functions without args without return
+# 0. import random # library
+# 1. create a function with name "randomizer"
+# 2. create a varieble with name "data" = 'your text at 6-10 words'
+# or "lorem ipsum..."
+# 3. select var "data" with text and with random method print 
+# with for-loop 20 times only ONE character
+
+data = 'Antidisestablishmentarianism'
+
+
+
+# randint() 	Returns a random number between the given range
+# choice() 	Returns a random element from the given sequence
+
+
+
+
+
+# functions with args
+
+
